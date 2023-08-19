5752639cd62679e35da3cd255c1d1f78401b6729
Asher Norland <asher.norland@gmail.com>
2023-08-18 20:21:37 -0400

    Add self option

--- porringer/application/click.py
+++ porringer/application/click.py
@@ -14,11 +14,21 @@ pass_config = click.make_pass_decorator(Configuration, ensure=True)
 
 
 @click.group()
-@click.option("-v", "--verbose", count=True, help="Print additional output")
+@click.option("-v", "--verbose", count=True, default=0, help="Print additional output")
 @click.version_option()
 @pass_config
-def application(config: Configuration) -> None:
+def application(config: Configuration, verbose: int) -> None:
     """entry_point group for the CLI commands
+
+    Args:
+        config: The CLI configuration object
+    """
+
+
+@application.group(invoke_without_command=True)
+@pass_config
+def self(config: Configuration) -> None:
+    """Command group to inspect Porringer itself
 
     Args:
         config: The CLI configuration object
