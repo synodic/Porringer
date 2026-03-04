ae679b8ac01c2909cd195e12c0d986377a6eeba7
Asher Norland <asher.norland@gmail.com>
2026-03-04 10:44:32 -0800

    JSON Schema Support

--- tests/unit/test_manifest.py
+++ tests/unit/test_manifest.py
@@ -659,6 +659,88 @@ class TestManifestSchema:
         assert 'preferences' in props
         assert 'post_sync' in props
 
+    @staticmethod
+    def test_manifest_schema_has_root_meta_fields() -> None:
+        """Exported schema contains $schema and $id root meta-fields"""
+        schema = SyncCommands.manifest_schema()
+
+        assert schema['$schema'] == 'https://json-schema.org/draft/2020-12/schema'
+        assert schema['$id'] == 'https://synodic.github.io/porringer/schema.json'
+
+    @staticmethod
+    def test_manifest_schema_has_dollar_schema_property() -> None:
+        """Exported schema exposes $schema as an optional property for editor support"""
+        schema = SyncCommands.manifest_schema()
+        props = schema['properties']
+
+        assert '$schema' in props
+        assert props['$schema']['type'] == 'string'
+        assert props['$schema']['format'] == 'uri'
+
+    @staticmethod
+    def test_manifest_schema_package_spec_anyof() -> None:
+        """PackageSpec definition uses anyOf to allow string shorthand"""
+        schema = SyncCommands.manifest_schema()
+        defs = schema.get('$defs', {})
+
+        assert 'PackageSpec' in defs
+        package_spec = defs['PackageSpec']
+        assert 'anyOf' in package_spec
+
+        type_kinds = [alt.get('type') for alt in package_spec['anyOf']]
+        assert 'string' in type_kinds
+
+    @staticmethod
+    def test_manifest_schema_plugin_spec_anyof() -> None:
+        """PluginSpec definition uses anyOf to allow string shorthand"""
+        schema = SyncCommands.manifest_schema()
+        defs = schema.get('$defs', {})
+
+        assert 'PluginSpec' in defs
+        plugin_spec = defs['PluginSpec']
+        assert 'anyOf' in plugin_spec
+
+        type_kinds = [alt.get('type') for alt in plugin_spec['anyOf']]
+        assert 'string' in type_kinds
+
+    @staticmethod
+    def test_manifest_schema_package_ref_anyof() -> None:
+        """PackageRef definition uses anyOf to allow string shorthand"""
+        schema = SyncCommands.manifest_schema()
+        defs = schema.get('$defs', {})
+
+        assert 'PackageRef' in defs
+        package_ref = defs['PackageRef']
+        assert 'anyOf' in package_ref
+
+        type_kinds = [alt.get('type') for alt in package_ref['anyOf']]
+        assert 'string' in type_kinds
+
+    @staticmethod
+    def test_manifest_accepts_dollar_schema_field() -> None:
+        """SetupManifest accepts $schema in input without raising ValidationError"""
+        manifest = SetupManifest.model_validate(
+            {
+                '$schema': 'https://synodic.github.io/porringer/schema.json',
+                'version': '1',
+                'packages': {'python': ['pytest']},
+            }
+        )
+
+        assert manifest.version == '1'
+        assert len(manifest.packages) == 1
+
+    @staticmethod
+    def test_manifest_rejects_unknown_extra_fields() -> None:
+        """SetupManifest still rejects arbitrary unknown fields (extra='forbid')"""
+        with pytest.raises(ValidationError):
+            SetupManifest.model_validate(
+                {
+                    'version': '1',
+                    'not_a_real_field': 'should fail',
+                }
+            )
+
 
 @pytest.mark.mock_packages
 class TestDryRunStateAware:
